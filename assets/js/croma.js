4969d1748a7ba8f3447b2499314d3edfee224c38
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-08-16 04:23:17 +0530

    Tweaked gradient and added event emitter

--- assets/js/croma.js
+++ assets/js/croma.js
@@ -22,12 +22,13 @@ var Croma = (function() {
 
                 sheet.canvas.width = $canvas.parent().innerWidth();
 
-                gradient.addColorStop(0,    "rgb(255,   0,   0)");
+                gradient.addColorStop(0.00, "rgb(255,   0,   0)");
                 gradient.addColorStop(0.15, "rgb(255,   0, 255)");
                 gradient.addColorStop(0.33, "rgb(0,     0, 255)");
                 gradient.addColorStop(0.49, "rgb(0,   255, 255)");
                 gradient.addColorStop(0.67, "rgb(0,   255,   0)");
                 gradient.addColorStop(0.84, "rgb(255, 255,   0)");
+                gradient.addColorStop(1.00, "rgb(255,   0,   0)");
 
                 sheet.fillStyle = gradient;
                 sheet.fillRect(0, 0, sheet.canvas.width, sheet.canvas.height);
