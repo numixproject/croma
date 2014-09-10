36b85d295c9dfab8b08e2e30c55ddc98e96dbdd4
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-09-10 20:14:09 +0200

    Added Croma to Android -Share with- menu.

--- src/main/java/com/numix/croma/MainActivity.java
+++ src/main/java/com/numix/croma/MainActivity.java
@@ -1,12 +1,15 @@
 package com.numix.croma;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.JavascriptInterface;
@@ -17,6 +20,8 @@ import android.webkit.WebViewClient;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import me.croma.image.Color;
@@ -28,7 +33,7 @@ public class MainActivity extends Activity {
 
     private final String index = "file:///android_asset/www/index.html";
 
-    private WebView webView;
+    public WebView webView;
 
     // Arbitrary integer to check request code
     private final int SELECT_PHOTO = 1;
@@ -48,30 +53,116 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.main);
 
-        webView = (WebView) findViewById(R.id.webview);
 
-        WebSettings webSettings = webView.getSettings();
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String action = intent.getAction();
 
-        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+// if this is from the share menu
+        if (Intent.ACTION_SEND.equals(action)) {
+            if (extras.containsKey(Intent.EXTRA_STREAM)) {
+                // Get resource path
+                Uri uri = (Uri) extras.getParcelable(Intent.EXTRA_STREAM);
 
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setSupportZoom(false);
-        webSettings.setSaveFormData(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setAppCachePath(appCachePath);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setCacheMode(LOAD_DEFAULT);
+                // what to do here
 
-        webView.setWebViewClient(new webViewClient());
+                String URL = index;
+                String SAVE_AS = parseUriToFilename(uri);
 
-        // Expose Java methods as JavaScript interfaces
-        webView.addJavascriptInterface(new Storage(this), "androidStorage");
-        webView.addJavascriptInterface(new CromaImage(this), "cromaImage");
+                try {
+                    final InputStream imageStream = getContentResolver().openInputStream(uri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
 
-        // Load the page to WebView
-        webView.loadUrl(index);
+                    BitMapImage b = new BitMapImage(selectedImage);
+
+                    KMeansColorPicker k = new KMeansColorPicker();
+
+                    URL = index + "#/show-palette?saveas=" + SAVE_AS + "&palette=";
+
+                    try {
+                        List<Color> l = k.getUsefulColors(b, 6);
+
+                        for (Color c : l) {
+                            URL += c.getRed() + "," + c.getGreen() + "," + c.getBlue() + "|";
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                webView = (WebView) findViewById(R.id.webview);
+
+                WebSettings webSettings = webView.getSettings();
+
+                String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+                webSettings.setSupportZoom(false);
+                webSettings.setSaveFormData(true);
+                webSettings.setDomStorageEnabled(true);
+                webSettings.setAppCacheEnabled(true);
+                webSettings.setAppCachePath(appCachePath);
+                webSettings.setAllowFileAccess(true);
+                webSettings.setCacheMode(LOAD_DEFAULT);
+
+                webView.setWebViewClient(new webViewClient());
+
+                // Expose Java methods as JavaScript interfaces
+                webView.addJavascriptInterface(new Storage(this), "androidStorage");
+                webView.addJavascriptInterface(new CromaImage(this), "cromaImage");
+                webView.loadUrl(URL);
+            }
+        } else {
+
+            webView = (WebView) findViewById(R.id.webview);
+
+            WebSettings webSettings = webView.getSettings();
+
+            String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+            webSettings.setSupportZoom(false);
+            webSettings.setSaveFormData(true);
+            webSettings.setDomStorageEnabled(true);
+            webSettings.setAppCacheEnabled(true);
+            webSettings.setAppCachePath(appCachePath);
+            webSettings.setAllowFileAccess(true);
+            webSettings.setCacheMode(LOAD_DEFAULT);
+
+            webView.setWebViewClient(new webViewClient());
+
+            // Expose Java methods as JavaScript interfaces
+            webView.addJavascriptInterface(new Storage(this), "androidStorage");
+            webView.addJavascriptInterface(new CromaImage(this), "cromaImage");
+
+            // Load the page to WebView
+            webView.loadUrl(index);
+        }
+    }
+
+    public String parseUriToFilename(Uri uri) {
+        String selectedImagePath = null;
+        String filemanagerPath = uri.getPath();
+
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            selectedImagePath = cursor.getString(column_index);
+        }
+
+        if (selectedImagePath != null) {
+            return selectedImagePath;
+        }
+        else if (filemanagerPath != null) {
+            return filemanagerPath;
+        }
+        return null;
     }
 
     // Handle back button press
@@ -85,6 +176,7 @@ public class MainActivity extends Activity {
 
         return super.onKeyDown(keyCode, event);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
@@ -126,6 +218,7 @@ public class MainActivity extends Activity {
                 webView.loadUrl(URL);
         }
     }
+
 
     public class CromaImage extends MainActivity {
 
