61f8462418e5030770d833fce37b788356937ce0
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-09-28 00:42:33 +0530

    remove log messages

--- app/src/main/java/org/numixproject/croma/MainActivity.java
+++ app/src/main/java/org/numixproject/croma/MainActivity.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.JavascriptInterface;
@@ -20,7 +19,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.anjlab.android.iab.v3.*;
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -126,7 +126,10 @@ public class MainActivity extends Activity implements BillingProcessor.IBillingH
 
         // Initialize billing processor
         bp = new BillingProcessor(this, getString(R.string.license_key), this);
+
+        // Restore purchases
         bp.loadOwnedPurchasesFromGoogle();
+
         // Set webview options
         setContentView(R.layout.main);
 
@@ -255,18 +258,13 @@ public class MainActivity extends Activity implements BillingProcessor.IBillingH
     }
 
     public class InAppBilling {
-        public InAppBilling() {
-
-        }
         @JavascriptInterface
         public void purchase(String productId) {
-            Log.d("purchase", productId);
             bp.purchase(productId);
         }
 
         @JavascriptInterface
         public String isPurchased(String productId) {
-            System.out.println("product ID" + productId);
             return bp.isPurchased(productId) ? "true": "false";
         }
     }
