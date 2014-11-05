5d5dac718b1292560d2061a210a4a9a0f1d05b1d
kamal <kkjoshi.it@gmail.com>
2014-11-06 00:49:48 +0530

    rotation animation

--- app/src/main/java/org/numixproject/croma/Orientation.java
+++ app/src/main/java/org/numixproject/croma/Orientation.java
@@ -7,6 +7,11 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.view.OrientationEventListener;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -15,6 +20,7 @@ class Orientation extends OrientationEventListener {
     private ImageButton doneButton;
     private Bitmap icon;
     private Context context;
+    private int preAngle = 90;
     public Orientation(Context context, ImageButton doneButton) {
         super(context);
         this.context = context;
@@ -31,9 +37,31 @@ class Orientation extends OrientationEventListener {
             //if ((angle <= 45 && angle >= 0) || (angle <= 360 && angle >= (360 - 45))) angle = i;
             if ( angle >= (270 - 45) && angle <= (360 - 45) || (angle >= 45 && angle <= (90 + 45))) angle += 180;
             angle %= 360;
-            Bitmap newIcon = Utils.rotate(icon, (angle));
+           /* Bitmap newIcon = Utils.rotate(icon, (angle));
             Drawable d = new BitmapDrawable(this.context.getResources(), newIcon);
             this.doneButton.setImageDrawable(d);
+           */
+            angle = angle > 180 ? -(360 - angle) : angle;
+            if (Math.abs(preAngle - angle) > 3) {
+                AnimationSet animSet = new AnimationSet(true);
+                animSet.setInterpolator(new DecelerateInterpolator());
+                animSet.setFillAfter(true);
+                animSet.setFillBefore(true);
+                animSet.setFillEnabled(true);
+                animSet.initialize(doneButton.getWidth(), doneButton.getHeight(), doneButton.getWidth(), doneButton.getHeight());
+                final RotateAnimation animRotate = new RotateAnimation(preAngle, angle,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+                animRotate.setDuration(500);
+                animRotate.setFillAfter(true);
+                animSet.addAnimation(animRotate);
+                doneButton.startAnimation(animSet);
+
+                //doneButton.animate();
+                preAngle = angle;
+            }
+
             System.out.println("Orientation changed" + i);
         }
 
