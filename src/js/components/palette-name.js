ff130ee27f421d5342eaed83a7474bb3cf4faeb9
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-12-01 22:04:22 +0530

    Add autocapitalize="on" to name input box

--- src/js/components/palette-name.js
+++ src/js/components/palette-name.js
@@ -14,7 +14,7 @@ App.PaletteNameRoute.render = function(state, model) {
         "<div class='card-item-header'>Add new palette</div>",
         "<div class='paper-input-container'>",
         "<input id='palette-name' type='text' placeholder='Enter a name for the palette'",
-        "class='card-item-input-block card-item-input-name paper-input' autofocus>",
+        "class='card-item-input-block card-item-input-name paper-input' autocapitalize='on' autofocus>",
         "<span class='paper-input-highlight'></span>",
         "<span class='paper-input-bar'></span>",
         "</div></div></div>"
