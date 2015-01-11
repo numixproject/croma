0ca0934481a133856ef6e1c676bdb1ec7a164838
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-01-12 01:38:36 +0530

    Add template for new palette page

--- src/js/components/palette-new.js
+++ src/js/components/palette-new.js
@@ -5,43 +5,11 @@ var App = require("../core/app.js"),
 
 App.PaletteNewRoute.tags = [ "new" ];
 
-App.PaletteNewRoute.render = function(state, model) {
-    var html = "";
-
-    html += "<div class='section-content-center'><div class='section-content-center-inner'>";
-
-    if (utils.getPalette(true)) {
-        html += [
-            "<div class='paper-button-container fx-come-in'>",
-            "<a data-action='getpalette' class='paper-button paper-button-block'>",
-            "Get palette from image",
-            "</a></div>"
-        ].join("");
-    }
-
-    html += [
-        "<div class='paper-button-container fx-come-in'>",
-        "<div class='paper-button paper-button-block' data-action='topicker'>",
-        "Get palette from color",
-        "</div></div>",
-        "<div class='paper-button-container fx-come-in'>",
-        "<div class='paper-button paper-button-block' data-action='topalettename'>",
-        "Add colors manually",
-        "</div></div>"
-    ].join("");
-
-    if (!utils.isPro()) {
-        html += [
-            "<div class='paper-button-container unlock fx-come-in'>",
-            "<a data-action='unlockpro' class='paper-button paper-button-block'>",
-            "Unlock pro",
-            "</a></div>"
-        ].join("");
-    }
-
-    html += "</div></div>";
-
-    return html;
+App.PaletteNewRoute.model = function(state) {
+    return {
+        getPalette: utils.getPalette(true),
+        isPro: utils.isPro()
+    };
 };
 
 App.PaletteNewRoute.afterRender = function(state) {
