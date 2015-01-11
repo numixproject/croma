bb7f71e0523f392efc53ad32ea89cd90ef4b9f91
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-01-12 00:57:58 +0530

    Added template for details page

--- src/js/components/details.js
+++ src/js/components/details.js
@@ -29,52 +29,9 @@ App.DetailsRoute.model = function(state) {
         { key: "Darkness", value:  parseFloat(colorObj.darkness()).toFixed(2) }
     ];
 
+    model.copy = utils.copyToClipboard(true);
+
     return model;
-};
-
-App.DetailsRoute.render = function(state, model) {
-    var html = "",
-        strings;
-
-    if (!model) {
-        html += [
-            "<div class='empty-area fx-fade-in'>",
-            "<a class='empty-area-action'>An error occured!</a>",
-            "</div>"
-        ].join("");
-
-        return html;
-    }
-
-    html = [
-        "<div class='card-item fx-come-in'>",
-        "<div class='card-item-color-item-large' style='background-color:" + model.hexVal + "'></div>",
-        "<div class='card-item-info-wrap'>"
-    ].join("");
-
-    strings = model.strings;
-
-    for (var i = 0, l = strings.length; i < l; i++) {
-        if (!strings[i].value) {
-            continue;
-        }
-
-        html += [
-            "<div class='card-item-info" + (utils.copyToClipboard(true) ? " fx-ripple card-item-info-action-copy' data-action='copy'" : "'") + ">",
-            "<span class='card-item-label'>" + strings[i].key + "</span>",
-            "<span class='card-item-value'>" + strings[i].value + "</span>",
-            "</div>"
-        ].join("");
-    }
-
-    html += [
-        "</div></div>",
-        "<div class='paper-button-container fx-come-in'>",
-        "<a data-action='topalettes' class='paper-button paper-button-block'>See color palettes</a>",
-        "</div>"
-    ].join("");
-
-    return html;
 };
 
 App.DetailsRoute.afterRender = function(state) {
