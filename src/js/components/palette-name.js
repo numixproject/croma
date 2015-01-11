8b0381ae9ab87a54887bf5dab156e68f6f4f341d
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-01-12 01:29:45 +0530

    Add template for palette name page

--- src/js/components/palette-name.js
+++ src/js/components/palette-name.js
@@ -5,24 +5,6 @@ var App = require("../core/app.js"),
 
 App.PaletteNameRoute.tags = [ "action" ];
 
-App.PaletteNameRoute.render = function(state, model) {
-    var html = "";
-
-    html += [
-        "<div class='card-item fx-come-in'>",
-        "<div class='card-item-container'>",
-        "<div class='card-item-header'>Add new palette</div>",
-        "<div class='paper-input-container'>",
-        "<input id='palette-name' type='text' placeholder='Enter a name for the palette'",
-        "class='card-item-input-block card-item-input-name paper-input' autocapitalize='on' autofocus>",
-        "<span class='paper-input-highlight'></span>",
-        "<span class='paper-input-bar'></span>",
-        "</div></div></div>"
-    ].join("");
-
-    return html;
-};
-
 App.PaletteNameRoute.afterRender = function(state) {
     var suggested = state.params ? state.params.suggested : null,
         $input = $("#palette-name");
