6d41aa042fe391379c810357c1c0d32345e071fd
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-02-22 17:47:50 +0100

    Fixes...

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -38,9 +38,9 @@ App.IndexRoute.model = function() {
     }
 
     return {
-	palettes: data.sort(utils.sortByDate),
-	share: !("external" in window && window.external && "getUnityObject" in window.external && window.external.getUnityObject("1.0"))
-	};
+        palettes: data.sort(utils.sortByDate),
+        share: !("external" in window && window.external && "getUnityObject" in window.external && window.external.getUnityObject("1.0"))
+    };
 };
 
 App.IndexRoute.afterRender = function() {
